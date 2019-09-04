c5d7e5a5a95d0d49dc148d143baef1ac1e96fd95
Jane <jane.degtiareva@gmail.com>
2019-09-04 16:15:09 -0700

    Update testing script

--- tests/run_vm_standalone_tests.sh
+++ tests/run_vm_standalone_tests.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 cargo install near-vm-runner-standalone
+
+TESTS=(base58Test base64Test logTest storageStringRoundtripTest storageBytesRoundtripTest storageGenericGetSetRoundtripTest storageKeysTest mapTests mapTestsWithPrimitices vectorTests dequeTests topnTests promiseTests mathTests)
+
+DISABLED_TESTS=(contextTests)
+
+for i in "${TESTS[@]}"
+do
+    # access each element  
+    # as $i 
+    echo $i 
+near-vm-runner-standalone --bin near-vm-runner-standalone -- --context-file=/Users/evgueniadegtiareva/near/r2/context.json --config-file=/Users/evgueniadegtiareva/near/r2/config.json --method-name=$i --wasm-file=/Users/evgueniadegtiareva/near/r2/out/main.wasm --input="{}"
+
+done
+
+#cargo run --package near-vm-runner-standalone --bin near-vm-runner-standalone -- --context-file=~/near/r2/context.json --config-file=~/near/r2/config.json --method-name=hello --wasm-file=/Users/evgueniadegtiareva/near/r2/out/main.wasm --input="{}"
+
